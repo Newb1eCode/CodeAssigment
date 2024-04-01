9e6de678afaf2f48f3a672ba94e54450ace6955e
Newb1eCode <tdaoxuan113@gmail.com>
2024-04-02 02:21:03 +0700

    Create SanPhamChiTiet_Repository.java

---
+++ src/main/java/com/whale/buoi_n_1/repositories/SanPhamChiTiet_Repository.java
@@ -0,0 +1,47 @@
+package com.whale.buoi_n_1.repositories;
+
+import com.whale.buoi_n_1.Utils.HibernateUtil;
+import com.whale.buoi_n_1.entities.SanPhamChiTiet;
+import jakarta.persistence.Query;
+import org.hibernate.Session;
+
+import java.util.List;
+
+public class SanPhamChiTiet_Repository {
+    private Session session;
+
+    public SanPhamChiTiet_Repository() {
+        this.session = HibernateUtil.getFACTORY().openSession();;
+    }
+
+    public List<SanPhamChiTiet> getAll() {
+        String hql = "SELECT entity FROM SanPhamChiTiet entity";
+        Query query = this.session.createQuery(hql, SanPhamChiTiet.class);
+        return query.getResultList();
+    }
+
+    public List<SanPhamChiTiet> findById (int id){
+        List<SanPhamChiTiet> listSpCTById = (List<SanPhamChiTiet>)
+                session.createQuery("from SanPhamChiTiet where sanPham.id = :id_1")
+                    .setParameter("id_1", id).list();
+
+        return listSpCTById;
+    }
+
+    public List<SanPhamChiTiet> findByTrangThai() {
+        List<SanPhamChiTiet> listSpCTById = (List<SanPhamChiTiet>)
+                session.createQuery("from SanPhamChiTiet where trangThai = 'Active'").list();
+        return listSpCTById;
+    }
+
+    public void createSpct (SanPhamChiTiet newSpct){
+        try {
+            session.getTransaction().begin();
+            session.persist(newSpct);
+            session.getTransaction().commit();
+        } catch (Exception e){
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+    }
+}
