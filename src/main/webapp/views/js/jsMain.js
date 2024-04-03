5fcf4d81bf375531cfd08e1392aca9ee3096e859
Newb1eCode <tdaoxuan113@gmail.com>
2024-04-03 11:43:12 +0700

    Create jsMain.js

---
+++ src/main/webapp/views/js/jsMain.js
@@ -0,0 +1,6 @@
+const myButton = document.getElementById("thanhToan");
+function handleClick(event) {
+    console.log("Hoa Don Da Duoc Thanh Toan");
+    alert("Thanh Toan Thanh Cong");
+}
+// myButton.addEventListener("click", handleClick);
